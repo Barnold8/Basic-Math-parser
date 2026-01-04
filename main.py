f0b64fdb969a5e9a900fa13c570c40f9d4599398
Barnold88 <bandslover@outlook.com>
2026-01-04 04:20:43 +0000

    Fix god awful tokeniser to now not reverse numbers to construct them and also allow negative numbers, however, postfixEval might be broken or infixToPostFix is

--- main.py
+++ main.py
@@ -1,36 +1,119 @@
 from typing import Any, List, Union
 
-def tokeniseEquation(equation:str) -> List[Any]: # currently broken for floats and doesnt even work for negative numbers...
+# def tokeniseEquation(equation:str) -> List[Any]: # currently broken for floats and doesnt even work for negative numbers...
+
+#     num = 0
+#     power = 0
+#     isFloat = False
+#     isNegative = False
+#     tokens = []
+
+#     # while i < len(equation[::-1]):
+#     #     if equation[i].isnumeric():
+#     #         num += -int(equation[i]) * (10** power)
+
+#     #     if equation[i] == '-1':
+#     #         pass
+#     #     else:
+#     #         if num != 0:
+#     #             tokens.append(num)
+#     #         tokens.append(equation[i])
+#     #         num = 0
+#     #         power = 0
+
+#     #     i += 1
+
+#     # if num != 0:
+#     #     tokens.append(num)
+
+#     i = 0
+
+#     for i, char in enumerate(reversed(equation)):
+        
+#         if char.isnumeric():
+           
+#             print(isNegative)
+
+#             if isNegative == True:
+#                 print(f"num before {num}")
+#                 num += -int(char) * (10** power)
+#                 print(f"num after{num}")
+#             else:
+#                 num += int(char) * (10** power)
+
+#         elif char == '-':
+#             print(str(i-1) + " | " + equation)
+#             if i+1 < len(equation): # this is just for debugging
+#                 print(f"In minus check for char ({equation[i-1]}){equation[i]}{equation[i+1]}")
+#             if i -1 <= 0:
+#                 print("Changing isNegative to True")
+#                 isNegative = True
+#                 print(f"isNegative is {isNegative}")
+#             elif i-1 > 0:
+#                 if equation[-1].isnumeric() == False:
+#                     print("Changing isNegative to True")
+#                     isNegative = True
+#                     print(f"isNegative is {isNegative}")
+#                 else:
+#                     print(f"{equation[-1]} is a number")
+#             # else:
+#             #     isNegative = False
+
+#             print(f"Is negative is {isNegative} by end of check")
+
+#         else:
+            
+#             if num != 0:
+#                 tokens.append(num)
+
+#             tokens.append(char)
+#             num = 0
+#             power = 0
+
+#     if num != 0:
+#         tokens.append(num)
+
+#     return tokens[::-1]
+
+def tokeniseEquation(equation:str) -> List[Any]: 
 
     num = 0
-    power = 0
-    isFloat = False
     tokens = []
+    isNegative = False
 
-    for i, char in enumerate(reversed(equation)):
+    for i, char in enumerate(equation):
+        
         if char.isnumeric():
-            
-            if isFloat:
-                num += int(char) * (10** -power)
+            if isNegative:
+                num = -(num * 10 + (int(char)))
             else:
-                num += int(char) * (10** power)
-            power += 1
-        elif char == '.':
+                num = num * 10 + (int(char))
+
+        elif char == '-':
             
-            if len(equation) > i and equation[i+1].isnumeric():
-                isFloat = True 
+            if i == 0:
+                isNegative = True
+            elif i - 1 >= 0:
+                if equation[i-1].isnumeric() == False:
+                    isNegative = True
+                else:
+                    if num != 0:    # Not following DRY here at all, but IT WORKS!!!
+                        tokens.append(num)
+                        num = 0
+                        isNegative = False
+                    tokens.append(char)
         else:
-            
             if num != 0:
                 tokens.append(num)
+                num = 0
+                isNegative = False
             tokens.append(char)
-            num = 0
-            power = 0
-
+            
     if num != 0:
         tokens.append(num)
 
-    return tokens[::-1]
+    return tokens
+
 
 def isNumeric(token:Any):
     return type(token) == float or type(token) == int
@@ -109,3 +192,13 @@ def evalPostFix(queue:list) -> Union[float, int]:
 
 def evalEquation(equation:str)-> Union[float, int]:
     return evalPostFix(infixToPostFix(tokeniseEquation(equation)))
+
+eq = "-1"
+eq1 = "(-1)"
+eq2 = "(-1+20*3-1)-1"
+# eq = "25"
+
+# print(tokeniseEquation(eq))
+# print(tokeniseEquation(eq1))
+print(tokeniseEquation(eq2))
+print(evalEquation(eq2))
